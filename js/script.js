7885a6c7ded257768a818488301b07abfa454155
Lavaj <lavajpathak1@gmail.com>
2025-11-11 23:44:02 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,6 @@
 // Example of interactive behavior (if you want to add future JS features)
 
 document.addEventListener('DOMContentLoaded', function () {
-    console.log("Welcome to Lavaj Portfolio!");
+    console.log("Welcome to Lavaj Pathak!");
   });
   
